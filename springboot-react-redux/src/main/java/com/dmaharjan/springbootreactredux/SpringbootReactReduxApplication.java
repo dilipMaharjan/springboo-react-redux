62638668ed84983c5f71b2885f3975d3d5a5e5ae
dilipMaharjan <dilipmaharjann@gmail.com>
2020-02-15 14:16:57 -0700

    Spring boot starter setup

---
+++ springboot-react-redux/src/main/java/com/dmaharjan/springbootreactredux/SpringbootReactReduxApplication.java
@@ -0,0 +1,13 @@
+package com.dmaharjan.springbootreactredux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactReduxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactReduxApplication.class, args);
+	}
+
+}
---
+++ springboot-react-redux/src/test/java/com/dmaharjan/springbootreactredux/SpringbootReactReduxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmaharjan.springbootreactredux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactReduxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
